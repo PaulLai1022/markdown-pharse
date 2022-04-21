01f302a84fbf7a10766edcb06c0c52a9bd5354fc
PaulLai1022 <71100835+PaulLai1022@users.noreply.github.com>
2022-04-21 10:39:43 -0700

    Junit setup
    javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,33 +1,33 @@
-//https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
-
+    
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
-
-            //break the while loop while we could find the next open bracket
-            if(openBracket == -1){
-                break;
-            }
             int closeBracket = markdown.indexOf("]", openBracket);
-
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
 
-            if(!markdown.substring(currentIndex, closeBracket).equals("!")){
-                currentIndex = closeBracket+1;
-                continue;
-
+            //fix 3: break loop if [, ], ( or ) is not found
+            //(fix 1 removed because redundant)
+            if(openBracket == -1 || closeBracket == -1 ||
+                openParen == -1 || closeParen == -1) {
+                    break;
             }
+
+             //fix 2: skip image links
+             if(markdown.substring(currentIndex, openBracket).contains("![")){
+                 currentIndex = closeParen + 1;
+                 continue;
+             }            
+
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
@@ -40,6 +40,7 @@ public class MarkdownParse {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        System.out.println(links);
+        System.out.println("Lab-4 Part 2 File Seperator");
     }
 }
